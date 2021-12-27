3d07687f064b1412e1a862ff67437a27dffbd4da
rmm <67968321+yuchi0307@users.noreply.github.com>
2021-12-27 21:39:34 +0800

    Update App.js

--- React Forms/App.js
+++ React Forms/App.js
@@ -1,11 +1,52 @@
-import SimpleInput from './components/SimpleInput';
+import { useState, useRef } from "react";
 
-function App() {
+const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameValid, setEnteredNameValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const inputRef = useRef();
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredNameValid(event.target.value);
+  };
+
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    setEnteredNameTouched(true);
+    if(enteredName.trim() === "")
+    {
+      setEnteredNameValid(false);
+      return; //如果輸入空值直接跳離此function，不會執行底下的 
+    }
+    setEnteredNameValid(true);
+    console.log(enteredName);
+
+    const look = inputRef.current.value;
+    console.log(look);
+    setEnteredName("");
+  };
+  const nameInputIsInvalid = enteredNameTouched && !enteredNameValid;
+
+  const nameInputClasses = nameInputIsInvalid ? "form-control invalid" : "form-control";
   return (
-    <div className="app">
-      <SimpleInput />
-    </div>
+    <form onSubmit={formSubmitHandler}>
+      <div className={nameInputClasses }>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={inputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+        {nameInputIsInvalid && <p className='error-text'>name can't be empty</p>}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
   );
-}
+};
 
-export default App;
+export default SimpleInput;
