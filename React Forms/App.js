71abaa65c277860b56cf6c2b0c1a53181dfb499b
rmm <67968321+yuchi0307@users.noreply.github.com>
2021-12-28 21:13:22 +0800

    將程式碼簡化

--- React Forms/App.js
+++ React Forms/App.js
@@ -1,32 +1,34 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameValid, setEnteredNameValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-  const inputRef = useRef();
+
+
+  const enteredNameIsValid = enteredName.trim() !=="";
+  const nameInputIsInvalid = enteredNameTouched && !enteredNameIsValid;
+  
 
   const nameInputChangeHandler = (event) => {
-    setEnteredNameValid(event.target.value);
+    setEnteredName(event.target.value);
   };
+
+  const nameInputBlurHandler = event =>{
+    setEnteredNameTouched(true);
+  }
 
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
     setEnteredNameTouched(true);
-    if(enteredName.trim() === "")
+   if(!enteredNameIsValid)
     {
-      setEnteredNameValid(false);
-      return; //如果輸入空值直接跳離此function，不會執行底下的 
+      return;
     }
-    setEnteredNameValid(true);
-    console.log(enteredName);
-
-    const look = inputRef.current.value;
-    console.log(look);
     setEnteredName("");
+    setEnteredNameTouched(false);
   };
-  const nameInputIsInvalid = enteredNameTouched && !enteredNameValid;
+ 
 
   const nameInputClasses = nameInputIsInvalid ? "form-control invalid" : "form-control";
   return (
@@ -34,10 +36,10 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses }>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={inputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          onBliur={nameInputBlurHandler}
           value={enteredName}
         />
         {nameInputIsInvalid && <p className='error-text'>name can't be empty</p>}
@@ -50,3 +52,4 @@ const SimpleInput = (props) => {
 };
 
 export default SimpleInput;
+
