60ae73ef48d6c85f409c15382e030c31968a4152
rmm <67968321+yuchi0307@users.noreply.github.com>
2022-01-05 22:37:09 +0800

    自主練習

--- React Forms/src/components/BasicForm.js
+++ React Forms/src/components/BasicForm.js
@@ -1,22 +1,57 @@
+import { useState } from "react";
+
 const BasicForm = (props) => {
+
+  const [enteredName, setEnteredName]= useState('');
+  const [nameInputIsTouched, setNameInputIsTouched] = useState(false);
+
+  const nameIsValid = enteredName.trim()!=="";
+  const nameIsInValid = nameInputIsTouched && !enteredName;
+
+  const enteredNameChangeHandler = event =>{
+    setEnteredName(event.target.value);
+  }
+  const nameInputBlur = event =>{
+    setNameInputIsTouched(true);
+  }
+
+  const nameInputClass = nameIsInValid ? 'form-control invalid':'form-control';
+ 
+  let formIsValid = false; //不用放到其他function內
+  if(nameIsValid){
+        formIsValid = true;
+      }
+
+  const formSubmitHandler = event =>{
+    event.preventDefault();
+    
+  }
+
+  
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
+    <form onSubmit={formSubmitHandler}> {/*確認鍵是設在表單tag，不是button*/}
+        <div className={nameInputClass}> {/* 在html tag中可以直接{}寫JSX */}
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input 
+            type='text' 
+            id='name' 
+            onChange={enteredNameChangeHandler}
+            onBlur={nameInputBlur}
+            value={enteredName}
+          />
+          {nameIsInValid && <p className='error-text'>name can't be empty!</p>}
         </div>
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
           <input type='text' id='name' />
         </div>
-      </div>
+
       <div className='form-control'>
         <label htmlFor='name'>E-Mail Address</label>
         <input type='text' id='name' />
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
