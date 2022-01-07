a51ce8adcba66c8d879a50b4b6a146f0c43e1229
rmm <67968321+yuchi0307@users.noreply.github.com>
2022-01-07 15:20:45 +0800

    Update BasicForm.js

--- React Forms/src/components/BasicForm.js
+++ React Forms/src/components/BasicForm.js
@@ -1,6 +1,9 @@
 //import { useState } from "react";
 import useInput from '../hooks/use-input';
 
+const isNotEmpty = value => value.trim()!==''; //這個function不會被rebuild,因此寫在component外
+const isEmail = value => value.includes('@');
+
 const BasicForm = (props) => {
 
       const {
@@ -8,46 +11,91 @@ const BasicForm = (props) => {
         //isTouched: firstNameIsTouched, 這是多餘的
         isValid: firstNameValid, //只需要這個
         inputChangeHandler: firstNameChanged,
-        inputBlurHandler: firstNameTouched,
+        inputBlurHandler: firstNameBlured,
         hasError: firstNameError,
         reset: resetEnteredFirstName,
-      } = useInput(value => value.trim()!=='')
+      } = useInput(isNotEmpty)
+
+      const {
+        value: enteredLastName,
+        isValid: lastNameValid, 
+        inputChangeHandler: lastNameChanged,
+        inputBlurHandler: lastNameBlured,
+        hasError: lastNameError,
+        reset: resetEnteredLastName,
+      } = useInput(isNotEmpty)
+
+      const {
+        value: enteredEmail,
+        isValid: emailValid, 
+        inputChangeHandler: emailChanged,
+        inputBlurHandler: emailBlured,
+        hasError: EmailError,
+        reset: resetEmail,
+      } = useInput(isEmail)
 
 
-  const nameInputClass = firstNameError ? 'form-control invalid':'form-control';
+  const firstNameClasses = firstNameError ? 'form-control invalid':'form-control';
+  const lastNameClasses = lastNameError ? 'form-control invalid':'form-control';
+  const emailClasses = EmailError ? 'form-control invalid':'form-control';
  
   let formIsValid = false; //不用放到其他function內
-  if(firstNameValid){
+  if(firstNameValid && lastNameValid && emailValid){
         formIsValid = true;
       }
 
   const formSubmitHandler = event =>{
     event.preventDefault();
+
+
+    if(!formIsValid) 
+    {
+      return;
+    }
+
+    console.log('submitted!')
+    console.log(enteredFirstName, enteredLastName, enteredEmail);
+    
     resetEnteredFirstName();
+    resetEnteredLastName();
+    resetEmail();
   }
 
   
   return (
     <form onSubmit={formSubmitHandler}> {/*確認鍵是設在表單tag，不是button*/}
-        <div className={nameInputClass}> {/* 在html tag中可以直接{}寫JSX */}
+        <div className={firstNameClasses}> {/* 在html tag中可以直接{}寫JSX */}
           <label htmlFor='name'>First Name</label>
           <input 
             type='text' 
             id='name' 
             onChange={firstNameChanged}
-            onBlur={firstNameTouched}
-            value={enteredFirstName}
+            onBlur={firstNameBlured}
+            value={enteredLastName}
           />
-          {firstNameError && <p className='error-text'>name can't be empty!</p>}
+          {firstNameError && <p className='error-text'>first name can't be empty!</p>}
         </div>
-        <div className='form-control'>
+        <div className={lastNameClasses}>
           <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+           <input 
+            type='text' 
+            id='name' 
+            onChange={lastNameChanged}
+            onBlur={lastNameBlured}
+            value={enteredLastName}
+          />
+          {lastNameError && <p className='error-text'>last name can't be empty!</p>}
         </div>
 
-      <div className='form-control'>
+      <div className={emailClasses}>
         <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+        <input type='text' 
+         id='email' 
+         value={enteredEmail}
+         onChange={emailChanged}
+         onBlur={emailBlured}
+         />
+          {emailError && <p className='error-text'>e-mail address should have @!</p>}
       </div>
       <div className='form-actions'>
         <button disabled={!formIsValid}>Submit</button>
