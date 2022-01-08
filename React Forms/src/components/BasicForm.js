5a66380309492cc4273af21104492155e02e2432
rmm <67968321+yuchi0307@users.noreply.github.com>
2022-01-08 10:46:05 +0800

    input can't type :(
    已經比對過老師的code, 除了變數命名其他都正確, 網路上的解法 onChange 也是寫對的, 還不知道為什麽欄位不能輸入
    老師的： https://github.com/academind/react-complete-guide-code/blob/16-working-with-forms/code/11-applying-our-hook-and-knowledge/src/components/BasicForm.js

--- React Forms/src/components/BasicForm.js
+++ React Forms/src/components/BasicForm.js
@@ -1,3 +1,4 @@
+
 //import { useState } from "react";
 import useInput from '../hooks/use-input';
 
@@ -30,15 +31,10 @@ const BasicForm = (props) => {
         isValid: emailValid, 
         inputChangeHandler: emailChanged,
         inputBlurHandler: emailBlured,
-        hasError: EmailError,
+        hasError: emailError,
         reset: resetEmail,
       } = useInput(isEmail)
 
-
-  const firstNameClasses = firstNameError ? 'form-control invalid':'form-control';
-  const lastNameClasses = lastNameError ? 'form-control invalid':'form-control';
-  const emailClasses = EmailError ? 'form-control invalid':'form-control';
- 
   let formIsValid = false; //不用放到其他function內
   if(firstNameValid && lastNameValid && emailValid){
         formIsValid = true;
@@ -46,7 +42,6 @@ const BasicForm = (props) => {
 
   const formSubmitHandler = event =>{
     event.preventDefault();
-
 
     if(!formIsValid) 
     {
@@ -60,10 +55,14 @@ const BasicForm = (props) => {
     resetEnteredLastName();
     resetEmail();
   }
-
+  const firstNameClasses = firstNameError ? 'form-control invalid':'form-control';
+  const lastNameClasses = lastNameError ? 'form-control invalid':'form-control';
+  const emailClasses = emailError ? 'form-control invalid':'form-control';
+ 
   
   return (
     <form onSubmit={formSubmitHandler}> {/*確認鍵是設在表單tag，不是button*/}
+      <div className='control-group'>
         <div className={firstNameClasses}> {/* 在html tag中可以直接{}寫JSX */}
           <label htmlFor='name'>First Name</label>
           <input 
@@ -71,7 +70,7 @@ const BasicForm = (props) => {
             id='name' 
             onChange={firstNameChanged}
             onBlur={firstNameBlured}
-            value={enteredLastName}
+            value={enteredFirstName}
           />
           {firstNameError && <p className='error-text'>first name can't be empty!</p>}
         </div>
@@ -86,7 +85,7 @@ const BasicForm = (props) => {
           />
           {lastNameError && <p className='error-text'>last name can't be empty!</p>}
         </div>
-
+      </div>
       <div className={emailClasses}>
         <label htmlFor='name'>E-Mail Address</label>
         <input type='text' 
