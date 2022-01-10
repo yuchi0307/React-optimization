ae0b872fd881cf240eb4320bac5d06cd67b5958e
rmm <67968321+yuchi0307@users.noreply.github.com>
2022-01-10 14:46:40 +0800

    Update use-input.js

--- React Forms/src/hooks/use-input.js
+++ React Forms/src/hooks/use-input.js
@@ -31,3 +31,59 @@ const useInput = (validateValue) => {
 };
 
 export default useInput;
+/*
+用useReducer寫:
+
+import { useState } from 'react';
+
+const initialInputState = {
+  value: '',
+  isTouched: false
+}
+
+const inputStateReducer = (state, action) =>{
+  if(action.type=='INPUT')
+  { return {value: action.value, isTouched: state.isTouched}
+
+  }
+  if(action.type=='BLUR')
+  {
+    return {isTouched: true}
+  }
+  if(action.type=='RESET')
+  {
+    return {value: '', isTouched: false}
+  }  
+  return inputStateReducer
+}
+
+const useInput = (validateValue) => {
+  const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState)
+
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
+
+  const inputChangeHandler = (event) => {
+    dispatch({type: 'INPUT', value: event.target.value})
+  };
+
+  const inputBlurHandler = (event) => {
+    dispatch({typr: 'BLUR'})
+  };
+
+  const reset = () => {
+    dispatch({type: 'RESET'})
+  };
+
+  return {
+    value: inputState.value,
+    isValid: valueIsValid,
+    hasError,
+    inputChangeHandler,
+    inputBlurHandler,
+    reset
+  };
+};
+
+export default useInput;
+*/
